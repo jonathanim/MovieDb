c46e46656ae270a1d4e4ff47bb273032a0e854a3
jonathanim <imjonathan.work@gmail.com>
2022-01-10 12:22:25 -0800

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavContainer = styled.div`
+height: 80px;
+background-color: black;
+color: white;
+display:flex;
+justify-content: space-between;
+`
+const NavItemWrap = styled.div`
+display: flex;
+justify-items: center;
+align-items: center;
+`
+const NavItem = styled.h1`
+margin: 10px 20px;
+ vertical-align: middle;
+`
+
+function Navbar() {
+    return (
+        <NavContainer>
+            <NavItemWrap>
+                <NavItem>
+                    MovieDB
+                </NavItem>
+            </NavItemWrap>
+
+            <NavItemWrap>
+                <NavItem>
+                    Popular
+                </NavItem>
+            </NavItemWrap>
+
+            <NavItemWrap>
+                <NavItem>
+                    Trending
+                </NavItem>
+            </NavItemWrap>
+            <NavItemWrap>
+                <NavItem>
+                    Netflix
+                </NavItem>
+            </NavItemWrap>
+            <NavItemWrap>
+                <NavItem>
+                    Hulu
+                </NavItem>
+            </NavItemWrap>
+            <NavItemWrap>
+                <NavItem>
+                    @Threater
+                </NavItem>
+            </NavItemWrap>
+        </NavContainer>
+    )
+}
+
+export default Navbar
